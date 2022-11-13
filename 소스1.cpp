d858feb51da2d82439bc6f000b9c4b055ec1553a
sonhyek jin <hjson817@mokpo.ac.kr>
2022-11-13 18:47:03 +0900

    초기 커밋.

---
+++ 소스1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+int dy[50];
+int main() {
+	ios_base::sync_with_stdio(false);
+	freopen("input.txt", "rt", stdin);
+
+	return 0;
+}
